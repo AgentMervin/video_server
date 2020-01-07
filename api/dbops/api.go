185553665041b09f245f885dec0ce6c73c1c0bb8
AgentMervin <3262469782@qq.com>
2020-01-06 21:41:43 -0800

    get create delete with credence

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -16,5 +16,36 @@ func AddUserCredential(loginName string, pwd string) error{
 		return err
 	}
 	_,err = stmtIns.Exec(loginName,pwd)
-
+    stmtIns.Close()
+	return nil
 }
+
+func GetUserCredential(loginName string)(string, error){
+	stmtOut, err := dbConn.Prepare("SELECT pwd FROM users WHERE login_name = ?")
+	if err !=nil{
+		log.Printf("%s", err)
+		return "", err
+	}
+	var pwd string
+	err = stmtOut.QueryRow(loginName).Scan(&pwd)
+	if err != nil && err != sql.ErrNoRows {
+		return "", err
+	}
+	defer stmtOut.Close()
+	return pwd, nil
+}
+
+func DeleteUser(loginName string, pwd string) error {
+	stmtDel, err :=dbConn.Prepare("DELETE FROM users WHERE login_name=? AND pwd=?")
+	if err != nil {
+		log.Printf("Delete user error: %s", err)
+	}
+	_, err = stmtDel.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+	defer stmtDel.Close()
+	return nil
+}
+
+func
