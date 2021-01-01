4d2a50e4d8e5dad8109e7b58e510263eb792bbb5
AgentMervin <3262469782@qq.com>
2021-01-01 02:41:03 -0800

    stream handler

---
+++ stream/defs.go
@@ -0,0 +1,6 @@
+package main
+
+const (
+	VIDEO_DIR = "./videos/"
+	//MAX_UPLOAD_SIZE = 1024 * 1024 * 50 //50MB
+)
---
+++ stream/handler.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	vid := p.ByName("vid-id")
+	vl := VIDEO_DIR + vid
+
+	video, err := os.Open(vl)
+	if err != nil {
+		log.Printf("Error when try to open file: %v", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+
+	w.Header().Set("Content-Type", "video/mp4")
+	http.ServeContent(w, r, "", time.Now(), video)
+
+	defer video.Close()
+}
