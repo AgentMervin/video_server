cf10861d9ee0265c8cbcf076663ac6d790495553
AgentMervin <3262469782@qq.com>
2021-01-01 00:25:35 -0800

    rate limit design

---
+++ stream/limiter.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+"log"
+)
+
+type ConnLimiter struct {
+	concurrentConn int
+	bucket chan int
+}
+
+func NewConnLimiter(cc int) *ConnLimiter {
+	return &ConnLimiter {
+		concurrentConn: cc,
+		bucket: make(chan int, cc),
+	}
+}
+
+func (cl *ConnLimiter) GetConn() bool {
+	if len(cl.bucket) >= cl.concurrentConn {
+		log.Printf("Reached the rate limitation.")
+		return false
+	}
+
+	cl.bucket <- 1
+	return true
+}
+
+func (cl *ConnLimiter) ReleaseConn() {
+	c :=<- cl.bucket
+	log.Printf("New connction coming: %d", c)
+}
