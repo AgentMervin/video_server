2859f3a0d8d82ff26e4a4cc1a3b2b01e070c3a1f
AgentMervin <3262469782@qq.com>
2021-01-05 00:45:10 -0800

    finish streaming test

---
+++ stream/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+
+type middleWareHandler struct {
+	r *httprouter.Router
+	l *ConnLimiter
+}
+
+func NewMiddleWareHandler(r *httprouter.Router, cc int) http.Handler {
+	m := middleWareHandler{}
+	m.r = r
+	m.l = NewConnLimiter(cc)
+	return m
+}
+
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("/videos/:vid-id", streamHandler)
+
+	return router
+}
+
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !m.l.GetConn() {
+		sendErrorResponse(w, http.StatusTooManyRequests, "Too many requests")
+		return
+	}
+
+	m.r.ServeHTTP(w, r)
+	defer m.l.ReleaseConn()
+}
+
+func main() {
+	r := RegisterHandlers()
+	mh := NewMiddleWareHandler(r, 2)
+	http.ListenAndServe(":9000", mh)
+}
