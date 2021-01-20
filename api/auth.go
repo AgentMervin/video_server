91552da6948da8ac61e27f113998ca75fe5ab82c
AgentMervin <3262469782@qq.com>
2021-01-19 16:19:19 -0800

    back to old auth method

---
+++ api/auth.go
@@ -0,0 +1,38 @@
+package main 
+
+import (
+	"net/http"
+	"github.com/avenssi/video_server/api/session"
+	"github.com/avenssi/video_server/api/defs"
+
+)
+
+var HEADER_FIELD_SESSION = "X-Session-Id"
+var HEADER_FIELD_UNAME = "X-User-Name"
+
+
+func validateUserSession(r *http.Request) bool {
+	sid := r.Header.Get(HEADER_FIELD_SESSION)
+	if len(sid) == 0 {
+		return false
+	}
+
+	uname, ok := session.IsSessionExpired(sid)
+	if ok {
+		return false
+	}
+
+	r.Header.Add(HEADER_FIELD_UNAME, uname)
+	return true
+}
+
+func ValidateUser(w http.ResponseWriter, r *http.Request) bool {
+	uname := r.Header.Get(HEADER_FIELD_UNAME)
+	if len(uname) == 0 {
+		sendErrorResponse(w, defs.ErrorNotAuthUser)
+		return false
+	}
+
+	return true
+}
+
---
+++ api/response.go
@@ -0,0 +1,20 @@
+package main 
+
+import (
+	"io"
+	"encoding/json"
+	"net/http"
+	"github.com/avenssi/video_server/api/defs"
+)
+
+func sendErrorResponse(w http.ResponseWriter, errResp defs.ErrResponse) {
+	w.WriteHeader(errResp.HttpSC)
+
+	resStr, _ := json.Marshal(&errResp.Error)
+	io.WriteString(w, string(resStr))
+}
+
+func sendNormalResponse(w http.ResponseWriter, resp string, sc int) {
+	w.WriteHeader(sc)
+	io.WriteString(w, resp)
+}
